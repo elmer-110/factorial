a05bb0d9344cd284945bbe90a8097d73707b8405
elmer-110 <ecedillov@student.universidadunie.com>
2025-03-17 21:28:54 +0100

    recursivo/ iterativo

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
+
+# Ejemplo de uso
+n = 10
+print(f"El número Fibonacci en la posición {n} es {fibonacci(n)}")
